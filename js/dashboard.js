6964a60c00f8bfcf1339da601bd05e0bb7bc6960
Jose Luis Somilleda <joseluissom004@gmail.com>
2022-03-13 12:59:29 -0600

    Se crean los metodos para obtener, modificar cards en main y dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,9 @@
+window.onload = function (){
+    getAllProducts()
+}
+
 const buildCard = (title, description, photoUrl, price, category, productId) => {
-    
+
     let cardCol = document.createElement("div");
     let cardContainer = document.createElement("div");
     let cardImage = document.createElement("img");
@@ -19,7 +23,7 @@ const buildCard = (title, description, photoUrl, price, category, productId) => 
     cardTextPrice.classList.add("card-text");
 
     //Add css
-    cardContainer.style.setProperty('width','15rem')
+    cardContainer.style.setProperty('width', '15rem')
 
 
     // Add values to the elements
@@ -47,7 +51,7 @@ const buildCard = (title, description, photoUrl, price, category, productId) => 
         category: category
     };
 
-    cardContainer.addEventListener('click',()=>{
+    cardContainer.addEventListener('click', () => {
         let myModal = new bootstrap.Modal(document.getElementById('dashboard-modal-edit-product'))
         myModal.show()
         setFielsModal(product);
@@ -96,6 +100,86 @@ const getProduct = (id) => {
         })
 }
 
-function setFielsModal(product){
-    document.getElementById('exampleInputEmail1').value = product.title
+const getAllProducts = () => {
+    const url = "https://kodecamp2022-ed27f-default-rtdb.firebaseio.com/products.json";
+
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((products) => {
+
+            if (products != null) {
+                let messageEmpty = document.getElementById('message-empty-products')
+                if (messageEmpty) {
+                    messageEmpty.style.setProperty('visibility', 'hidden');
+                }
+
+
+                for (const key in products) {
+                    const product = products[key]
+                    const card = buildCard(product.title, product.description, product.imageUrl, product.price, product.category, key)
+                    mainContent.appendChild(card)
+                }
+            } else {
+                let emptyProducts = document.createElement("h1");
+                emptyProducts.id = "message-empty-products";
+                emptyProducts.innerText = "Aun no se cuentan con productos"
+                mainContent.appendChild(emptyProducts);
+            }
+        })
+}
+
+const updateProduct = (title, description, photoUrl, price, category, productId) => {
+    const url = `https://kodecamp2022-ed27f-default-rtdb.firebaseio.com/products/${productId}.json`;
+
+    const product = {
+        title: title,
+        description: description,
+        price: price,
+        imageUrl: photoUrl,
+        category: category
+    };
+
+    fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+    }).then((res) => {
+        if (res.ok) {
+            let alertSuccess = document.createElement("div");
+            let iconSuccess = document.createElement("i");
+            let spanSuccess = document.createElement("span");
+
+            spanSuccess.innerText = `The product has been successfully updated`;
+
+            alertSuccess.classList.add("alert", "alert-success");
+            iconSuccess.classList.add("fas", "fa-check-circle");
+
+            mainContent.appendChild(alertSuccess)
+            alertSuccess.appendChild(iconSuccess);
+            alertSuccess.appendChild(spanSuccess);
+
+        } else {
+            let alertError = document.createElement("div");
+            let iconError = document.createElement("i");
+            let spanError = document.createElement("span");
+
+            spanError.innerText = `The product could not be updated, please try again later`;
+
+            alertError.classList.add("alert", "alert-Error");
+            iconError.classList.add("fas", "fa-exclamation-circle");
+
+            mainContent.appendChild(alertError)
+            alertError.appendChild(iconError);
+            alertError.appendChild(spanError);
+        }
+    });
+};
+
+function setFielsModal(product) {
+    console.log(product)
+    document.getElementById('exampleInputEmailEdit').value = product.title
 }
--- js/main.js
+++ js/main.js
@@ -1,29 +1,192 @@
 'use strict';
 
-window.onload = function (){
+window.onload = function () {
     initializeComponents()
+    getAllProductsByCategory()
 }
 
-function initializeComponents(){
-    document.getElementById('nav-shopping-cart').addEventListener('click',()=>{
+function initializeComponents() {
+    document.getElementById('nav-shopping-cart').addEventListener('click', () => {
         displayShoppinCart();
     });
 
-    document.getElementById('shopping-cart-close').addEventListener('click',() =>{
+    document.getElementById('shopping-cart-close').addEventListener('click', () => {
         hiddenShoppingCart();
     });
 }
 
-function displayShoppinCart(){
+function displayShoppinCart() {
     let shoppingCart = document.getElementById('aside-shopping-cart')
     let percentReduceScrean = window.screen.height * .2
     let screen = window.screen.height - percentReduceScrean
-    shoppingCart.style.setProperty('height',  `${screen}px`)
-    shoppingCart.style.setProperty('visibility','visible');
+    shoppingCart.style.setProperty('height', `${screen}px`)
+    shoppingCart.style.setProperty('visibility', 'visible');
 }
 
 
-function hiddenShoppingCart(){
+function hiddenShoppingCart() {
     let shoppingCart = document.getElementById('aside-shopping-cart')
-    shoppingCart.style.setProperty('visibility','hidden');
+    shoppingCart.style.setProperty('visibility', 'hidden');
+}
+
+const buildCardMain = (title, description, photoUrl, price, category, productId) => {
+
+    let cardCol = document.createElement("div");
+    let cardContainer = document.createElement("div");
+    let cardImage = document.createElement("img");
+    let cardBody = document.createElement("div");
+    let cardTitle = document.createElement("h5");
+    let cardTextDescription = document.createElement("p");
+    let cardTextPrice = document.createElement("p");
+    let cardDivBtnFull = document.createElement("div");
+    let cardBtnAdd = document.createElement("p");
+    let cardBtnIcon = document.createElement("i");
+
+
+    // Add classes to elements
+    cardCol.classList.add("col-3");
+    cardContainer.classList.add("card");
+    cardImage.classList.add("card-img-top");
+    cardBody.classList.add("card-body");
+    cardTitle.classList.add("card-title");
+    cardTextDescription.classList.add("card-text");
+    cardTextPrice.classList.add("card-text");
+    cardDivBtnFull.classList.add("d-grid", "gap-2")
+    cardBtnAdd.classList.add("btn", "btn-primary")
+    cardBtnIcon.classList.add("fas", "fa-cart-plus")
+
+    //Add css
+    cardContainer.style.setProperty('width', '15rem')
+
+
+    // Add values to the elements
+    cardImage.src = photoUrl;
+    cardTitle.innerText = title;
+    cardTextDescription.innerText = description;
+    cardTextPrice.innerText = `Price: $ ${price}`;
+    cardBtnAdd.innerText = "Add Cart";
+
+    // Build structure
+    cardCol.appendChild(cardContainer);
+
+    cardContainer.appendChild(cardImage);
+    cardContainer.appendChild(cardBody);
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardTextDescription);
+    cardBody.appendChild(cardTextPrice);
+    cardBody.appendChild(cardDivBtnFull);
+
+    cardDivBtnFull.appendChild(cardBtnAdd);
+
+    cardBtnAdd.appendChild(cardBtnIcon);
+
+    const product = {
+        title: title,
+        description: description,
+        price: price,
+        imageUrl: photoUrl,
+        category: category,
+        productId: productId
+    };
+
+    cardBtnAdd.addEventListener('click', () => {
+        console.log(product);
+    });
+
+    return cardCol;
+};
+
+const buildProduct = (title, description, photoUrl, price, category, productId) => {
+    const objetcProduct = {
+        title: title,
+        description: description,
+        price: price,
+        imageUrl: photoUrl,
+        category: category,
+        productId: productId
+    };
+    return objetcProduct
+}
+
+const getAllProductsByCategory = () => {
+    const url = "https://kodecamp2022-ed27f-default-rtdb.firebaseio.com/products.json";
+
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((products) => {
+            let emptyProductShirts = document.createElement("h1");
+            emptyProductShirts.id = "message-empty-products-shirts";
+            emptyProductShirts.innerText = "Aun no se cuentan con productos"
+
+            let emptyProductsPants = document.createElement("h1");
+            emptyProductsPants.id = "message-empty-products-pants";
+            emptyProductsPants.innerText = "Aun no se cuentan con productos"
+
+            let emptyProductsSweater = document.createElement("h1");
+            emptyProductsSweater.id = "message-empty-products-sweater";
+            emptyProductsSweater.innerText = "Aun no se cuentan con productos"
+
+            let shirtsContent = document.getElementById("products-body-carts-shirts");
+            let pantsContent = document.getElementById("products-body-carts-pants");
+            let sweaterContent = document.getElementById("products-body-carts-sweater");
+
+            let shirtsArray = []
+            let pantsArray = []
+            let sweatersArray = []
+
+            if (products != null) {
+                for (const key in products) {
+                    const product = products[key]
+
+                    if (product.category == 1) {
+                        shirtsArray.push(buildProduct(product.title, product.description, product.imageUrl, product.price, product.category, key))
+                    } else if (product.category == 2) {
+                        pantsArray.push(buildProduct(product.title, product.description, product.imageUrl, product.price, product.category, key))
+                    } else if (product.category == 3) {
+                        sweatersArray.push(buildProduct(product.title, product.description, product.imageUrl, product.price, product.category, key))
+                    }
+                }
+
+                if (shirtsArray.length != 0) {
+                    let messageEmpty = document.getElementById('message-empty-products-shirts')
+                    if (messageEmpty) {
+                        messageEmpty.style.setProperty('visibility', 'hidden');
+                    }
+
+                    shirtsArray.forEach(product =>
+                        shirtsContent.appendChild(buildCardMain(product.title, product.description, product.imageUrl, product.price, product.category, product.productId))
+
+                    );
+
+                } else {
+                    shirtsContent.appendChild(emptyProductShirts);
+                }
+
+                if (pantsArray.length != 0) {
+                    let messageEmpty = document.getElementById('message-empty-products-shirts')
+                    if (messageEmpty) {
+                        messageEmpty.style.setProperty('visibility', 'hidden');
+                    }
+                } else {
+                    pantsContent.appendChild(emptyProductsPants);
+                }
+
+                if (sweatersArray.length != 0) {
+                    let messageEmpty = document.getElementById('message-empty-products-shirts')
+                    if (messageEmpty) {
+                        messageEmpty.style.setProperty('visibility', 'hidden');
+                    }
+                } else {
+                    sweaterContent.appendChild(emptyProductsSweater);
+                }
+
+            } else {
+                shirtsContent.appendChild(emptyProductShirts);
+                pantsContent.appendChild(emptyProductsPants);
+                sweaterContent.appendChild(emptyProductsSweater);
+            }
+        })
 }
